8c70ae7759573e4d17bf9dbc568fa68cb3bdbbb5
Wahaj Choudhry <wahaj@Wahajs-MacBook-Pro.local>
2024-09-27 22:25:26 +0500

    Added condition to check user role

--- compnents/EventListener.js
+++ compnents/EventListener.js
@@ -4,8 +4,9 @@ import { useDailyEvent } from "@daily-co/daily-react";
 export function CallEvents() {
   const [meetingState, setMeetingState] = useState("loading");
   useDailyEvent("app-message", (ev) => {
-    console.log("RECEIVED APP MESSAGE", ev);
-    setMeetingState(ev?.data?.properties?.speech);
+    console.log("EV", ev);
+    if (ev?.data?.properties?.role === "replica")
+      setMeetingState(ev?.data?.properties?.speech);
   });
 
   return (
