4ee2b9aa55f394426515b84c01214c805c9b9b0b
AjayKumarMP <ajaykumarmp145@gmail.com>
2024-09-28 09:26:23 +0530

    changes for background video

--- pages/conversation.js
+++ pages/conversation.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import MeadiaStream from "../compnents/meadiaStream";
 import Webcam from "react-webcam";
 import { GestureRecognizer, FilesetResolver } from "@mediapipe/tasks-vision";
 import { DrawingUtils } from "@mediapipe/tasks-vision";
-import MeadiaStream from "../compnents/meadiaStream";
-import Zoom from "@mui/material/Zoom";
-import Grow from "@mui/material/Grow";
-import { Button } from "@mui/material";
 import { useSnackbar } from "notistack";
 import { useCallObject } from "@daily-co/daily-react";
 import { useRouter } from "next/router";
+import { FaCheckCircle } from "react-icons/fa";
+import Grow from "@mui/material/Grow";
+import { translations } from "../utils/translatoins";
+import { AiOutlineLogin, AiOutlineLoading3Quarters } from "react-icons/ai";
+import CircularProgress from '@mui/material/CircularProgress';
 
 let gestureRecognizer;
 let runningMode = "VIDEO";
@@ -18,46 +20,42 @@ const Conversation = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const [loading, setLoading] = useState(true);
+  const [joining, setJoining] = useState(false);
+  const [handSensorLoaded, setHandSensorLoaded] = useState(false);
   const [dmessage, setMessage] = useState("");
   const [conversationId, setConversationId] = useState(false);
   let prev = "";
-  const [messages, setMessages] = useState("");
   const [started, setStarted] = useState(false);
   const [checked, setChecked] = useState(true);
   const [session, setSession] = useState({
     id: "",
     name: "",
   });
+  const [cameraPermission, setCameraPermission] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
   const router = useRouter();
   let callObject = useCallObject({
     options: {
-      // audioSource: mediaStreamTrack,
       startAudioOff: false,
-      // url: "https://tavus.daily.co/c74a52c6",
       userName: "Ajay",
     },
   });
 
   async function HandlJoin({ conversation_url }) {
     setLoading(true);
-    console.log("===---", conversation_url);
     try {
-      // await callObject?.leave()
       const res = await callObject?.join({ url: conversation_url });
 
-      console.log(res, "res");
       setLoading(false);
-      // enableSignLanguage()
       setTimeout(() => {
         Object.values(callObject.participants()).forEach((user) => {
-          console.log(callObject.participants(), "part", user);
           if (user.owner) {
             setSession({
               id: user.session_id,
               name: user.user_name,
             });
-            setLoading(false);
+            setStarted(true);
+            setJoining(false);
           }
         });
       }, 3000);
@@ -68,6 +66,25 @@ const Conversation = () => {
       setLoading(false);
     }
   }
+
+  useEffect(() => {
+    navigator.permissions
+      .query({ name: "camera" })
+      .then((permission) => {
+        setCameraPermission(permission.state === "granted");
+      })
+      .catch((error) => {
+        console.error("Permission error: ", error);
+      });
+
+    return () => {
+      const stream = videoRef?.current?.srcObject;
+      if (stream) {
+        const tracks = stream.getTracks();
+        tracks.forEach((track) => track.stop());
+      }
+    };
+  }, []);
 
   useEffect(() => {
     setTimeout(() => setChecked(true), 500);
@@ -84,7 +101,7 @@ const Conversation = () => {
         runningMode: runningMode,
         numHands: 2,
       });
-      setLoading(false);
+      setHandSensorLoaded(true);
     };
 
     loadGestureRecognizer();
@@ -99,14 +116,16 @@ const Conversation = () => {
       }
     };
   }, []);
-  let debounceTimeout;
+
   const enableCam = async () => {
     if (!gestureRecognizer) {
-      console.log("test", gestureRecognizer);
-      alert("Please wait for gestureRecognizer to load");
+      enqueueSnackbar("Gesture Recognizer is not present", {
+        variant: "error",
+      });
+
       return;
     }
-    setStarted(true);
+    setJoining(true);
     makeConversation();
 
     webcamRunning = !webcamRunning;
@@ -155,13 +174,7 @@ const Conversation = () => {
         const drawingUtils = new DrawingUtils(canvasCtx);
 
         if (results.landmarks) {
-          if (results?.gestures[0]?.categoryName) {
-            console.log(
-              results.gestures[0].categoryName,
-              results.gestures[0].displayName,
-              "=====",
-            );
-          }
+
           for (const landmarks of results.landmarks) {
             drawingUtils.drawConnectors(
               landmarks,
@@ -178,44 +191,24 @@ const Conversation = () => {
 
         // Display gesture results (e.g., category and score)
         if (results.gestures.length > 0) {
-          const categoryName = results.gestures[0][0].categoryName;
+          let categoryName = results.gestures[0][0].categoryName;
+          const length = results.gestures.length
+          if (length === 2) {
+            if (results.gestures[0][0].categoryName === results.gestures[1][0].categoryName) {
+              categoryName = results.gestures[0][0].categoryName + "_" + length
+            } else {
+              categoryName = results.gestures[0][0].categoryName + "_" + results.gestures[1][0].categoryName
+            }
+          }
           const categoryScore = parseFloat(
             results.gestures[0][0].score * 100,
           ).toFixed(2);
           if (categoryName !== "None" && categoryScore > 30) {
-            let message = "";
-            switch (categoryName) {
-              case "Victory":
-                message = "I'm Fine";
-                break;
-              case "Thumb_Down":
-                message = "No, Please";
-                break;
-              case "Thumb_Up":
-                message = "Ok, Fine";
-                break;
-              case "Closed_Fist":
-                message = "I'm Good";
-                break;
-              case "Open_Palm":
-                message = "Hello, How are you?";
-                break;
-              case "Pointing_Up":
-                message = "Thank you";
-                break;
-              case "ILoveYou":
-                message = "Can you check my Bank Balance please?";
-                break;
-            }
-            if (prev !== message) {
+            let message = translations[categoryName]
+            if (message && prev !== message) {
               prev = message;
-              console.log(message, "-----");
               setMessage(message);
             }
-
-            // console.log(
-            //   `Gesture: ${categoryName}, Confidence: ${categoryScore}%`
-            // );
           }
         }
       }
@@ -242,10 +235,10 @@ const Conversation = () => {
     };
     try {
       const response = await fetch(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': apiKey,
+          "Content-Type": "application/json",
+          "x-api-key": apiKey,
         },
         body: JSON.stringify(data),
       });
@@ -254,7 +247,6 @@ const Conversation = () => {
       }
       const responseData = await response.json();
       setConversationId(responseData.conversation_id);
-      console.log(responseData);
       setTimeout(() => {
         HandlJoin(responseData);
       }, 2000);
@@ -287,112 +279,138 @@ const Conversation = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <div className="w-full bg-gradient-to-br from-[#000] to-[#6366F1] text-white py-4 px-6 shadow-md flex justify-between items-center">
+      <div style={{ zIndex: 9 }} className="w-full bg-gradient-to-br from-[#000] to-[#6366F1] text-white py-4 px-6 shadow-md flex justify-between items-center">
         <h1 className="text-xl font-semibold">Hawking Hands</h1>
       </div>
-      {!started ? (
-        <Zoom in style={{ transitionDelay: "100ms" }}>
-          <video autoPlay muted loop height="100%">
+      <div className="min-h-screen flex justify-center items-center bg-gray-900 text-white gap-4">
+        {!started &&
+          // <Zoom in style={{ transitionDelay: "100ms" }}>
+          <video autoPlay muted loop height="100%" style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            minWidth: '110%',
+            width: 'auto',
+            height: 'auto',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 0, // Send the video to the back
+          }}>
             <source src="/videos/sign-video.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-        </Zoom>
-      ) : (
-        <div className="flex-grow w-full flex justify-center items-center p-6 lg:p-5 bg-gray-100">
-          <div className="relative w-full max-w-screen-lg flex h-full gap-4">
-            <div className="relative w-1/2 lg:h-[420px]">
-              <Webcam
-                className="w-full h-full object-cover rounded-lg"
-                ref={videoRef}
-                videoConstraints={{
-                  width: 1280,
-                  height: 720,
-                  facingMode: "user",
-                }}
-              />
-              <canvas
-                ref={canvasRef}
-                className="absolute top-0 left-0 w-full h-full"
-              />
-            </div>
-            <div className="relative w-1/1 lg:h-[480px]">
+          // </Zoom>
+        }
+        <div className="flex justify-center w-11/12 gap-12 align-middle" style={{ zIndex: 9 }}>
+          {started && (
+            <div className="w-4/5 h-full rounded-md bg-gray-800">
               <MeadiaStream
                 signText={dmessage}
                 callObject={callObject}
                 session={session}
-                loading={loading}
+                loading={!started}
               />
             </div>
+          )}
+          <div
+            className={`${started ? "w-2/5" : "w-1/3 min-w-[400px] bg-gray-800 shadow-lg"
+              }  rounded-xl ${started
+                ? "p-0 flex flex-col align-middle justify-center"
+                : "px-8 py-6"
+              }`}
+          >
+            {/* Header */}
+            {!started && (
+              <Grow
+                in={checked}
+                style={{ transformOrigin: "0 0 0" }}
+                {...(checked ? { timeout: 10000 } : {})}
+              >
+                <header className="flex items-center justify-between mb-6">
+                  <h1 className="text-xl font-semibold tracking-wide">
+                    Join the Conversation
+                  </h1>
+                  <button
+                    className={`${cameraPermission && handSensorLoaded && !joining
+                      ? "bg-gradient-to-r from-blue-500 to-teal-400 hover:from-blue-600 hover:to-teal-500"
+                      : "bg-gray-600 cursor-not-allowed"
+                      } text-white py-2 px-5 rounded-md shadow-md transition-transform duration-300 transform hover:scale-105 flex items-center space-x-2`}
+                    onClick={enableCam}
+                    disabled={!cameraPermission || !handSensorLoaded || joining}
+                  >
+                    <>
+                      <span>Enter</span>
+                      <AiOutlineLogin className="text-xl" />
+                      {joining &&<CircularProgress size="20px" />}
+                    </>
+                  </button>
+                </header>
+              </Grow>
+            )}
+            {/* Camera Preview */}
+            <div
+              className={`w-full h-48 bg-gray-700 rounded-md overflow-hidden relative  shadow-md flex items-center justify-center ${started ? "mb-0 h-60" : "mb-6"
+                }`}
+            >
+              {cameraPermission ? (
+                <>
+                  <Webcam
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                    ref={videoRef}
+                    videoConstraints={{
+                      width: 1280,
+                      height: 720,
+                      facingMode: "user",
+                    }}
+                  />
+                  <canvas
+                    ref={canvasRef}
+                    className="absolute top-0 left-0 w-full h-full"
+                  />
+                </>
+              ) : (
+                <div className="text-center space-y-4">
+                  <p className="text-lg font-medium">Camera Access Required</p>
+                </div>
+              )}
+            </div>
+
+            {/* Checklist Section */}
+            {!started && (
+              <div className="space-y-4">
+                {/* Camera Permission Check */}
+                <div className="flex items-center justify-between bg-gray-700 p-4 rounded-md shadow-md h-16">
+                  <span className="text-sm">Allow Camera Access</span>
+                  {cameraPermission ? (
+                    <FaCheckCircle className="text-green-500 text-2xl" />
+                  ) : (
+                    <FaCheckCircle className="text-grey-500 text-2xl" />
+                  )}
+                </div>
+
+                {/* Hand Motion Sensor Check */}
+                <div className="flex items-center justify-between bg-gray-700 p-4 rounded-md shadow-md h-16">
+                  <span className="text-sm">Loading Hand Motion Sensor</span>
+                  {handSensorLoaded ? (
+                    <FaCheckCircle className="text-green-500 text-2xl" />
+                  ) : (
+                    <AiOutlineLoading3Quarters className="text-yellow-500 text-2xl animate-spin" />
+                  )}
+                </div>
+              </div>
+            )}
+            {started && (
+              <footer className="mt-6 flex justify-center">
+                <button
+                  className="bg-red-500 hover:bg-red-600 py-3 px-8 rounded-md shadow-md focus:outline-none transition-transform duration-300 transform hover:scale-105 text-lg font-semibold"
+                  onClick={leaveConversation}
+                >
+                  Leave Conversation
+                </button>
+              </footer>
+            )}
+            {/* Footer */}
           </div>
         </div>
-      )}
-      <div className="w-full flex justify-center py-4 bg-gray-100">
-        {started ? (
-          <button
-            className="text-white bg-[#EF4444] hover:bg-[#EF4444-500 transition-all py-2 px-8 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            onClick={() => leaveConversation()}
-            aria-label="Stop the webcam"
-          >
-            Leave Conversation
-          </button>
-        ) : (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100vh",
-              textAlign: "center",
-              position: "absolute",
-              top: "0%",
-              color: "white", // Default text color
-              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)", // Text shadow for contrast
-            }}
-          >
-            <Grow
-              in={checked}
-              style={{ transformOrigin: "0 0 0" }}
-              {...(checked ? { timeout: 3000 } : {})}
-            >
-              <h1 style={{ fontSize: "25px", marginBottom: "10px" }}>
-                Where Technology Meets Everyone
-              </h1>
-            </Grow>
-            <Grow
-              in={checked}
-              style={{ transformOrigin: "0 0 0" }}
-              {...(checked ? { timeout: 6000 } : {})}
-            >
-              <p style={{ marginBottom: "20px" }}>
-                Connecting People and Technology
-              </p>
-            </Grow>
-            <Grow
-              in={checked}
-              style={{ transformOrigin: "0 0 0" }}
-              {...(checked ? { timeout: 10000 } : {})}
-            >
-              <Button
-                style={{
-                  marginTop: "20px",
-                  backgroundColor: "hsl(206.4, 100%, 42%)",
-                  color: "white",
-                  padding: "10px 20px",
-                  borderRadius: "8px",
-                  boxShadow: "0 2px 4px rgba(0,0,0,0.2)",
-                  transition: "background-color 0.3s",
-                }}
-                onClick={enableCam}
-                onMouseEnter={(e) => (e.currentTarget.style.width = "60%")}
-                onMouseLeave={(e) => (e.currentTarget.style.width = "55%")}
-                aria-label="Start the conversation"
-              >
-                Start Conversation
-              </Button>
-            </Grow>
-          </div>
-        )}
       </div>
     </div>
   );
